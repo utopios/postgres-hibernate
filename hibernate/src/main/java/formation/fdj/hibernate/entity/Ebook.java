782c1488571797a5dc66ee5494a7a7cb66d84636
ihab abadi <ihab@utopios.net>
2022-03-22 14:24:40 +0100

    polymotphic

--- hibernate/src/main/java/formation/fdj/hibernate/entity/Ebook.java
+++ hibernate/src/main/java/formation/fdj/hibernate/entity/Ebook.java
@@ -1,5 +1,8 @@
 package formation.fdj.hibernate.entity;
 
+import org.hibernate.annotations.Polymorphism;
+import org.hibernate.annotations.PolymorphismType;
+
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.PrimaryKeyJoinColumn;
@@ -7,5 +10,6 @@ import javax.persistence.PrimaryKeyJoinColumn;
 @Entity
 @DiscriminatorValue("ebook")
 @PrimaryKeyJoinColumn(name= "ebook_id")
+@Polymorphism(type = PolymorphismType.EXPLICIT)
 public class Ebook extends Book{
 }
